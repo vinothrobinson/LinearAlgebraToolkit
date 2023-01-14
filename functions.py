3cf7f9f0984113644c1205c7470eb870257a1e52
Karl <saderk@mcmaster.ca>
2023-01-14 17:39:15 -0500

    implemented 3/5 matrix functions

--- functions.py
+++ functions.py
@@ -14,6 +14,7 @@ class Matrix:
                 self.matrix[i][j] *= scalar
 
     def matrix_addition(self, other):
+        #newMatrix = Matrix(self.nun)
         if (self.num_of_cols != other.num_of_cols) or (self.num_of_rows != other.num_of_rows):
             return
         for i in range(num_of_rows):
@@ -22,10 +23,11 @@ class Matrix:
 
     def matrix_mult(self, other):
         if (self.num_of_cols != other.num_of_rows) or (self.num_of_rows != other.num_of_cols):
+            return
         newMatrix = Matrix(self.num_of_rows, other.num_of_cols)
         for i in range(self.num_of_rows): # Initializing all the elements in the matrix to 0
-            for i in range(other.num_of_cols):
-                newMatrix[i][j] = 0
+            for j in range(other.num_of_cols):
+                newMatrix[i].append(0)
         for i in range(self.num_of_rows): # Matrix Multiplication computation
             for j in range(self.num_of_cols):
                 newMatrix.matrix[i][j] += self.matrix[i][j] * other.matrix[j][i]
