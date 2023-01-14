87fa63341737c97c0a1ff849eb99dc667c037791
Karl <saderk@mcmaster.ca>
2023-01-14 17:30:47 -0500

    implemented 3/5 matrix functions

--- functions.py
+++ functions.py
@@ -5,19 +5,35 @@ class Matrix:
     def __init__(self, num_of_rows: int, num_of_cols: int) -> None:
         self.matrix = [[] * num_of_rows]
         self.isSquare = num_of_rows == num_of_cols
+        self.num_of_cols = num_of_rows
+        self.num_of_cols = num_of_cols
 
     def scalar_mult(self, scalar: float):
-        pass
+        for i in range(num_of_rows):
+            for j in range(num_of_cols):
+                self.matrix[i][j] *= scalar
 
     def matrix_addition(self, other):
-        pass
+        if (self.num_of_cols != other.num_of_cols) or (self.num_of_rows != other.num_of_rows):
+            return
+        for i in range(num_of_rows):
+            for j in range(num_of_cols):
+                self.matrix[i][j] += other.matrix[i][j]
 
-    def matrix_mult(self):
-        pass
+    def matrix_mult(self, other):
+        if (self.num_of_cols != other.num_of_rows) or (self.num_of_rows != other.num_of_cols):
+        newMatrix = Matrix(self.num_of_rows, other.num_of_cols)
+        for i in range(self.num_of_rows): # Initializing all the elements in the matrix to 0
+            for i in range(other.num_of_cols):
+                newMatrix[i][j] = 0
+        for i in range(self.num_of_rows): # Matrix Multiplication computation
+            for j in range(self.num_of_cols):
+                newMatrix.matrix[i][j] += self.matrix[i][j] * other.matrix[j][i]
 
     def det(self) -> float:
         if not self.isSquare:
             return
+        
 
     def get_eigenvalues(self) -> float:
         if not self.isSquare:
