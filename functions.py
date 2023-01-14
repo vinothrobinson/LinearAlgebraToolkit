4db8a628886105323c58de5cfc8f81d8e5ef4e16
Bani <banisomo2003@gmail.com>
2023-01-14 17:09:35 -0500

    outline

--- functions.py
+++ functions.py
@@ -1,18 +1,28 @@
-def scalar_mult():
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
-    print("karl")
+from tkinter import ttk
 
 
-def matrix_addition():
-    print('hi')
+class Matrix:
+    def __init__(self, num_of_rows: int, num_of_cols: int) -> None:
+        self.matrix = [[] * num_of_rows]
+        self.isSquare = num_of_rows == num_of_cols
+
+    def scalar_mult(self, scalar: float):
+        pass
+
+    def matrix_addition(self, other):
+        pass
+
+    def matrix_mult(self):
+        pass
+
+    def det(self) -> float:
+        if not self.isSquare:
+            return
+
+    def get_eigenvalues(self) -> float:
+        if not self.isSquare:
+            return
+
+
+class Window:
+    pass
